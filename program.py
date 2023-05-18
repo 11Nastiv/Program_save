b499618cd89d815c6c902ba160c4e6e8d118204b
Anastasiia Ivinskikh <nastia110495@mail.ru>
2023-05-18 17:21:49 +0800

    Changed 1

--- program.py
+++ program.py
@@ -7,7 +7,8 @@ class EventHandler(FileSystemEventHandler):
     def on_modified(self, event):
         for filename in os.listdir(folder_track):
             extension = filename.split(".")
-            if len(extension) > 1 and (extension[1].lower() == "jpg" or extension[1].lower() == "png" or extension[1].lower() == "pdf"):
+            if len(extension) > 1 and (extension[1].lower() == "jpg" or \
+            extension[1].lower() == "png" or extension[1].lower() == "pdf"):
                 file = folder_track + "/" + filename
                 new_path = folder_dest + "/" + filename
                 os.rename(file, new_path)
